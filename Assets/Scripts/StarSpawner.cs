536f5393d7e7716de3bee9a190036b36fd907704
PatrickGharib <patrick.a.gharib@gmail.com>
2020-01-28 18:25:34 -0700

    added "planet Spawing" currently hard coded to 1500 and lags at 10000 objects

---
+++ Assets/Scripts/StarSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSpawner : MonoBehaviour
+{
+    public Transform star;
+    public GameObject parentToSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i< 1500; i++)
+        {
+            var temp = ( Instantiate(star, new Vector3(Random.Range(0, 300), Random.Range(0, 300), Random.Range(0, 300)), Quaternion.identity));
+            temp.transform.parent = parentToSet.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Stars.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stars : MonoBehaviour
+{
+    public float mass;
+    public float acceleration;
+    private float MASS_MIN = 10000000;
+    private float MASS_MAX = 9999999999999999999;
+
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mass = Random.Range( MASS_MIN, MASS_MAX);
+        float starScale = Random.RandomRange(1, 7);
+        transform.localScale = new Vector3(starScale, starScale,starScale);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
